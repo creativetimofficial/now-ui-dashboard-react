732ec5d241c11f59892f351caeb5c2e573fede36
Manu <emanuelnazare@yahoo.ro>
2017-11-06 19:05:27 +0200

    added animation to notifications

--- src/views/Notifications/NotificationSystem/NotificationSystem.jsx
+++ src/views/Notifications/NotificationSystem/NotificationSystem.jsx
@@ -4,6 +4,8 @@ import {
     Col
 } from 'reactstrap';
 
+import "./NotificationSystem.css";
+
 class NotificationSystem extends React.Component{
     constructor(props) {
         super(props);
@@ -16,31 +18,59 @@ class NotificationSystem extends React.Component{
         this.onDismiss = this.onDismiss.bind(this);
         this.notify = this.notify.bind(this);
     }
-    onDismiss(nNumber){
+    onDismiss(nNumber,ye){
         var notify = [];
+        var sNotify;
         for(var i = 0 ; i < this.state.notify.length; i++){
             if(this.state.notify[i].key!==nNumber+""){
-                notify.push(this.state.notify[i]);
+                // notify.push(this.state.notify[i]);
+                if(ye===undefined){
+                    sNotify = React.cloneElement(
+                        this.state.notify[i],
+                        {className: "alert-with-icon animated moveUp"}
+                    );
+                }
+                else {
+                    sNotify = React.cloneElement(
+                        this.state.notify[i],
+                        {className: "alert-with-icon"}
+                    );
+                }
+                // sNotify.className += " animated fadeOutUp";
+                notify.push(sNotify);
+                console.log(sNotify);
+            } else {
+                if(ye===undefined){
+                    sNotify = React.cloneElement(
+                        this.state.notify[i],
+                        {className: "alert-with-icon animated fadeOutUp"}
+                    );
+                    // sNotify.className += " animated fadeOutUp";
+                    notify.push(sNotify);
+                    console.log(sNotify);
+
+                }
             }
         }
+        if(ye===undefined){
+            setTimeout(function(){this.onDismiss(nNumber,"ye")}.bind(this),800);
+        }
+
         this.setState({notify: notify});
+
     }
     notify(options){
         var notify = this.state.notify;
         var topleft = this.state.topleft;
         var nNumber = notify.length;
-        if(this.refs.notifications.childNodes[0] !== undefined){
-            topleft += this.refs.notifications.childNodes[this.refs.notifications.childNodes.length-1].clientHeight-8;
+        if(this.state.notify.length > 0){
             nNumber = parseInt(this.state.notify[this.state.notify.length-1].key,10)+1;
         }
-
         notify.push(
-            <Col xs={11} sm={4} style={{display: "inline-block", margin: "0px auto", position: "fixed", transition: "all 0.5s ease-in-out", zIndex: "1031", top: topleft+"px", left: "20px"}} key={nNumber}>
-                <Alert color="info" className="alert-with-icon" closeClassName="now-ui-icons ui-1_simple-remove" toggle={() => this.onDismiss(nNumber)}>
+                <Alert color="info" className="alert-with-icon animated fadeInDown" closeClassName="now-ui-icons ui-1_simple-remove" toggle={() => this.onDismiss(nNumber)} key={nNumber} >
                     <span data-notify="icon" className="now-ui-icons ui-1_bell-53"></span>
                     <span data-notify="message">{options.message}</span>
                 </Alert>
-            </Col>
         )
         this.setState({
             notify: notify,
@@ -51,9 +81,11 @@ class NotificationSystem extends React.Component{
         return (
             <div ref="notifications">
                 {
-                    this.state.notify.map((prop,key)=>{
+                    <Col xs={11} sm={4} id="hey">
+                    {this.state.notify.map((prop,key)=>{
                         return prop;
-                    })
+                    })}
+                    </Col>
                 }
             </div>
         );
