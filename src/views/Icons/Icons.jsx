c5fae53796ee0cfc639b42dced2afaf2d73c6afa
Manu <emanuelnazare@yahoo.ro>
2017-11-09 12:59:14 +0200

    small changes in icons view

--- src/views/Icons/Icons.jsx
+++ src/views/Icons/Icons.jsx
@@ -16,23 +16,23 @@ class Icons extends React.Component{
                             <CardHeader>
                                 <CardTitle>100 Awesome Nucleo Icons</CardTitle>
                                 <CardSubtitle>Handcrafted by our friends from <a href="https://nucleoapp.com/?ref=creativetim">NucleoApp</a>.</CardSubtitle>
-                                <CardBody className="all-icons">
-                                    <Row>
-                                        {
-                                            icons.map((prop,key) => {
-                                                return (
-                                                    <Col lg={2} md={3} sm={4} xs={6} className="font-icon-list" key={key}>
-                                                        <div className="font-icon-detail">
-                                                            <i className={"now-ui-icons " + prop}></i>
-                                                            <p>{prop}</p>
-                                                        </div>
-                                                    </Col>
-                                                );
-                                            })
-                                        }
-                                    </Row>
-                                </CardBody>
                             </CardHeader>
+                            <CardBody className="all-icons">
+                                <Row>
+                                    {
+                                        icons.map((prop,key) => {
+                                            return (
+                                                <Col lg={2} md={3} sm={4} xs={6} className="font-icon-list" key={key}>
+                                                    <div className="font-icon-detail">
+                                                        <i className={"now-ui-icons " + prop}></i>
+                                                        <p>{prop}</p>
+                                                    </div>
+                                                </Col>
+                                            );
+                                        })
+                                    }
+                                </Row>
+                            </CardBody>
                         </Card>
                     </Col>
                 </Row>
