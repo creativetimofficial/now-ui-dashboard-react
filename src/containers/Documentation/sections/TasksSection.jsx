27d37549c81454e50bbbbf32449333e944f4986b
Manu <emanuelnazare@yahoo.ro>
2017-11-10 10:28:27 +0200

    tasks doc is now done

--- src/containers/Documentation/sections/TasksSection.jsx
+++ src/containers/Documentation/sections/TasksSection.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { monokaiSublime } from 'react-syntax-highlighter/dist/styles';
 
-import Task from 'components/Task/Task.jsx';
+import Tasks from 'components/Tasks/Tasks.jsx';
 
-const codeImport = `import Task from 'components/Task/Task.jsx';`;
-const codeExample = `<table className="table">
-    <Task />
-</table>`;
+import { tasks } from 'variables/general.jsx';
+
+const codeImport = `import Tasks from 'components/Tasks/Tasks.jsx';
+import { tasks } from 'variables/general.jsx';`;
+const codeExample = `<Tasks tasks={tasks}/>`;
 class TasksSection extends React.Component{
     render(){
         return (
@@ -15,20 +16,16 @@ class TasksSection extends React.Component{
                 <div className="tim-row">
                     <h2>Tasks</h2>
                     <legend></legend>
-                    <SyntaxHighlighter language="javascript" style={monokaiSublime}>
-                        {codeImport}
-                    </SyntaxHighlighter>
                     <p>We've created a special component to render the tasks list shown in <code>Dashboard</code> <code>Task</code> card.</p>
                     <SyntaxHighlighter language="javascript" style={monokaiSublime}>
                         {codeImport}
                     </SyntaxHighlighter>
                     <h3>Example code</h3>
-                    <table className="table">
-                        <Task />
-                    </table>
+                    <Tasks tasks={tasks} />
                     <SyntaxHighlighter language="html" style={monokaiSublime}>
                         {codeExample}
                     </SyntaxHighlighter>
+                    <p>In this example we've passed our own tasks in the <code>tasks</code> prop which you'll find in <code>src/variables/general.jsx</code>. This prop has to be an array of strings.</p>
                     <p>If you want to understand this component better, please take a look inside <code>src/components/Tasks/Tasks.jsx</code> file.</p>
                 </div>
             </div>
