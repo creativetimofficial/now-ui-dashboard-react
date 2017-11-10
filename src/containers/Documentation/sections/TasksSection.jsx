216d03e9b7e55108e5c59b57273fd7ae791cb22e
Manu <emanuelnazare@yahoo.ro>
2017-11-10 10:06:10 +0200

    started to work on tasks doc

--- src/containers/Documentation/sections/TasksSection.jsx
+++ src/containers/Documentation/sections/TasksSection.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { monokaiSublime } from 'react-syntax-highlighter/dist/styles';
 
-const codeImport = ``;
+import Task from 'components/Task/Task.jsx';
 
-const codeExample = ``;
-
+const codeImport = `import Task from 'components/Task/Task.jsx';`;
+const codeExample = `<table className="table">
+    <Task />
+</table>`;
 class TasksSection extends React.Component{
     render(){
         return (
@@ -16,6 +18,18 @@ class TasksSection extends React.Component{
                     <SyntaxHighlighter language="javascript" style={monokaiSublime}>
                         {codeImport}
                     </SyntaxHighlighter>
+                    <p>We've created a special component to render the tasks list shown in <code>Dashboard</code> <code>Task</code> card.</p>
+                    <SyntaxHighlighter language="javascript" style={monokaiSublime}>
+                        {codeImport}
+                    </SyntaxHighlighter>
+                    <h3>Example code</h3>
+                    <table className="table">
+                        <Task />
+                    </table>
+                    <SyntaxHighlighter language="html" style={monokaiSublime}>
+                        {codeExample}
+                    </SyntaxHighlighter>
+                    <p>If you want to understand this component better, please take a look inside <code>src/components/Tasks/Tasks.jsx</code> file.</p>
                 </div>
             </div>
         );
