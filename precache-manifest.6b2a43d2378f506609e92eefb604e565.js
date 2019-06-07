cbb9968d289fe58e145637c6b001d81d33a24706
Manu <emanuelnazare@yahoo.ro>
2019-06-07 15:44:46 +0300

    Updates

---
+++ precache-manifest.6b2a43d2378f506609e92eefb604e565.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f23430db396730871d878096af11cf",
+    "url": "/now-ui-dashboard-react/index.html"
+  },
+  {
+    "revision": "b305a418396cb2fec0a0",
+    "url": "/now-ui-dashboard-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ee430236b08659fc2fb3",
+    "url": "/now-ui-dashboard-react/static/css/main.3075f6f8.chunk.css"
+  },
+  {
+    "revision": "b305a418396cb2fec0a0",
+    "url": "/now-ui-dashboard-react/static/js/2.1afb7660.chunk.js"
+  },
+  {
+    "revision": "ee430236b08659fc2fb3",
+    "url": "/now-ui-dashboard-react/static/js/main.fec0b486.chunk.js"
+  },
+  {
+    "revision": "5c5c1d61a25fa76a6548",
+    "url": "/now-ui-dashboard-react/static/js/runtime~main.6231c9ca.js"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/now-ui-dashboard-react/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/now-ui-dashboard-react/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/now-ui-dashboard-react/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
