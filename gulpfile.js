4528c58062cd20ecf04c428705709849d31f2429
EINazare <emanuelioannazare@gmail.com>
2020-06-15 11:50:16 +0300

    docs: add CT copyrights on build

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,29 @@ gulp.task("licenses", async function () {
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
+    // this is to add Creative Tim licenses in the production mode for the minified js
+    gulp
+      .src("build/static/js/runtime*.js", { base: "./" })
+      .pipe(
+        gap.prependText(`/*!
+
+  =========================================================
+  * Now UI Dashboard React - v1.4.0
+  =========================================================
+
+  * Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+  * Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+  * Coded by Creative Tim
+
+  =========================================================
+
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  */`)
+      )
+      .pipe(gulp.dest("./", { overwrite: true }));
+
   // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
