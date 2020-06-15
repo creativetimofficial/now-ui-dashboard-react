3f29e343b4271cc8675ca55aca48eda8f06ef28e
EINazare <emanuelioannazare@gmail.com>
2020-06-15 11:43:32 +0300

    docs: change versions and dates

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,11 +1,11 @@
 /*!
 
 =========================================================
-* Now UI Dashboard React - v1.2.0
+* Now UI Dashboard React - v1.4.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
@@ -106,7 +106,7 @@ class Dashboard extends React.Component {
             <Col xs={12} md={4}>
               <Card className="card-chart">
                 <CardHeader>
-                  <h5 className="card-category">2018 Sales</h5>
+                  <h5 className="card-category">2020 Sales</h5>
                   <CardTitle tag="h4">All products</CardTitle>
                   <UncontrolledDropdown>
                     <DropdownToggle
