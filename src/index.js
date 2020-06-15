a41185395a1490fada1d0a5c0aff96fe3478e91d
EINazare <emanuelioannazare@gmail.com>
2020-06-15 11:57:38 +0300

    refactor: change scripts

--- src/index.js
+++ src/index.js
@@ -19,8 +19,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createHashHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import ReactGA from "react-ga";
-import ReactPixel from "react-facebook-pixel";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/now-ui-dashboard.scss?v1.4.0";
@@ -30,22 +28,6 @@ import AdminLayout from "layouts/Admin.js";
 import DocumentationLayout from "layouts/Documentation/Documentation.js";
 
 const hist = createHashHistory();
-
-ReactPixel.init("111649226022273");
-ReactPixel.pageView();
-ReactPixel.fbq("track", "PageView");
-
-ReactGA.initialize("UA-46172202-1");
-ReactGA.set({ page: window.location.pathname + window.location.search });
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-hist.listen(location => {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
-  ReactPixel.pageView();
-  ReactPixel.fbq("track", "PageView");
-});
 
 ReactDOM.render(
   <Router history={hist}>
