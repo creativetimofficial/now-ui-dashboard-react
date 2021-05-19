bde6101f697ea8b7bc7e88ae13a3d386d181b3da
EINazare <emanuelioannazare@gmail.com>
2021-05-19 16:06:43 +0300

    refactor: change class components to functional ones

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -26,77 +26,72 @@ import logo from "logo-white.svg";
 
 var ps;
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.activeRoute.bind(this);
-  }
+function Sidebar(props) {
+  const sidebar = React.useRef();
   // verifies if routeName is the one active (in browser input)
-  activeRoute(routeName) {
-    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
-  }
-  componentDidMount() {
+  const activeRoute = (routeName) => {
+    return props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+  React.useEffect(() => {
     if (navigator.platform.indexOf("Win") > -1) {
-      ps = new PerfectScrollbar(this.refs.sidebar, {
+      ps = new PerfectScrollbar(sidebar.current, {
         suppressScrollX: true,
         suppressScrollY: false,
       });
     }
-  }
-  componentWillUnmount() {
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps.destroy();
-    }
-  }
-  render() {
-    return (
-      <div className="sidebar" data-color={this.props.backgroundColor}>
-        <div className="logo">
-          <a
-            href="https://www.creative-tim.com?ref=nudr-sidebar"
-            className="simple-text logo-mini"
-            target="_blank"
-          >
-            <div className="logo-img">
-              <img src={logo} alt="react-logo" />
-            </div>
-          </a>
-          <a
-            href="https://www.creative-tim.com?ref=nudr-sidebar"
-            className="simple-text logo-normal"
-            target="_blank"
-          >
-            Creative Tim
-          </a>
-        </div>
-        <div className="sidebar-wrapper" ref="sidebar">
-          <Nav>
-            {this.props.routes.map((prop, key) => {
-              if (prop.redirect) return null;
-              return (
-                <li
-                  className={
-                    this.activeRoute(prop.layout + prop.path) +
-                    (prop.pro ? " active active-pro" : "")
-                  }
-                  key={key}
-                >
-                  <NavLink
-                    to={prop.layout + prop.path}
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    <i className={"now-ui-icons " + prop.icon} />
-                    <p>{prop.name}</p>
-                  </NavLink>
-                </li>
-              );
-            })}
-          </Nav>
-        </div>
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+      }
+    };
+  });
+  return (
+    <div className="sidebar" data-color={props.backgroundColor}>
+      <div className="logo">
+        <a
+          href="https://www.creative-tim.com?ref=nudr-sidebar"
+          className="simple-text logo-mini"
+          target="_blank"
+        >
+          <div className="logo-img">
+            <img src={logo} alt="react-logo" />
+          </div>
+        </a>
+        <a
+          href="https://www.creative-tim.com?ref=nudr-sidebar"
+          className="simple-text logo-normal"
+          target="_blank"
+        >
+          Creative Tim
+        </a>
       </div>
-    );
-  }
+      <div className="sidebar-wrapper" ref="sidebar">
+        <Nav>
+          {props.routes.map((prop, key) => {
+            if (prop.redirect) return null;
+            return (
+              <li
+                className={
+                  this.activeRoute(prop.layout + prop.path) +
+                  (prop.pro ? " active active-pro" : "")
+                }
+                key={key}
+              >
+                <NavLink
+                  to={prop.layout + prop.path}
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  <i className={"now-ui-icons " + prop.icon} />
+                  <p>{prop.name}</p>
+                </NavLink>
+              </li>
+            );
+          })}
+        </Nav>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
