969211d5a50adc53cbd2472545d18df944b548c6
EINazare <emanuelioannazare@gmail.com>
2021-05-19 14:26:16 +0300

    refactor: delete history and use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -17,8 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/now-ui-dashboard.scss?v1.4.0";
@@ -26,14 +25,12 @@ import "assets/css/demo.css";
 
 import AdminLayout from "layouts/Admin.js";
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
+  <BrowserRouter>
     <Switch>
       <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
       <Redirect to="/admin/dashboard" />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
