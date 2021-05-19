6135fe2ece472187d939c6ffeb6f981ddf5ae41e
EINazare <emanuelioannazare@gmail.com>
2021-05-19 14:26:02 +0300

    fix: solve react google maps to simple Google Maps API

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -16,13 +16,6 @@
 
 */
 import React from "react";
-// react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
 
 // reactstrap components
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
@@ -30,93 +23,122 @@ import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
 // core components
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
 
-const MapWrapper = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }],
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ffffff" }, { lightness: 17 }],
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }],
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 18 }],
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 16 }],
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }],
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "#dedede" }, { lightness: 21 }],
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              { visibility: "on" },
-              { color: "#ffffff" },
-              { lightness: 16 },
-            ],
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              { saturation: 36 },
-              { color: "#333333" },
-              { lightness: 40 },
-            ],
-          },
-          { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{ color: "#f2f2f2" }, { lightness: 19 }],
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#fefefe" }, { lightness: 20 }],
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }],
-          },
-        ],
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
+const MapWrapper = () => {
+  const mapRef = React.useRef(null);
+  React.useEffect(() => {
+    let google = window.google;
+    let map = mapRef.current;
+    let lat = "40.748817";
+    let lng = "-73.985428";
+    const myLatlng = new google.maps.LatLng(lat, lng);
+    const mapOptions = {
+      zoom: 13,
+      center: myLatlng,
+      scrollwheel: false,
+      zoomControl: true,
+      styles: [
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry",
+          stylers: [{ color: "#f5f5f5" }, { lightness: 20 }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#ffffff" }, { lightness: 17 }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "geometry",
+          stylers: [{ color: "#ffffff" }, { lightness: 18 }],
+        },
+        {
+          featureType: "road.local",
+          elementType: "geometry",
+          stylers: [{ color: "#ffffff" }, { lightness: 16 }],
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [{ color: "#f5f5f5" }, { lightness: 21 }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#dedede" }, { lightness: 21 }],
+        },
+        {
+          elementType: "labels.text.stroke",
+          stylers: [
+            { visibility: "on" },
+            { color: "#ffffff" },
+            { lightness: 16 },
+          ],
+        },
+        {
+          elementType: "labels.text.fill",
+          stylers: [
+            { saturation: 36 },
+            { color: "#333333" },
+            { lightness: 40 },
+          ],
+        },
+        { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#f2f2f2" }, { lightness: 19 }],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#fefefe" }, { lightness: 20 }],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }],
+        },
+      ],
+    };
+
+    map = new google.maps.Map(map, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: "Now UI Dashboard React!",
+    });
+
+    const contentString =
+      '<div class="info-window-content"><h2>Now UI Dashboard React</h2>' +
+      "<p>A free Admin for React, Reactstrap, and React Hooks.</p></div>";
+
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker);
+    });
+  });
+  return (
+    <>
+      <div style={{ height: `100%` }} ref={mapRef}></div>
+    </>
+  );
+};
 
 class FullScreenMap extends React.Component {
   render() {
@@ -134,12 +156,7 @@ class FullScreenMap extends React.Component {
                     className="map"
                     style={{ position: "relative", overflow: "hidden" }}
                   >
-                    <MapWrapper
-                      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-                      loadingElement={<div style={{ height: `100%` }} />}
-                      containerElement={<div style={{ height: `100%` }} />}
-                      mapElement={<div style={{ height: `100%` }} />}
-                    />
+                    <MapWrapper />
                   </div>
                 </CardBody>
               </Card>
