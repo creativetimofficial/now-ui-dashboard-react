dafd2f39bcacb2bd62643117b6bca4943c3a567a
EINazare <emanuelioannazare@gmail.com>
2021-05-19 16:06:43 +0300

    refactor: change class components to functional ones

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,64 +21,62 @@ import { Container } from "reactstrap";
 // used for making the prop types of this component
 import PropTypes from "prop-types";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer
-        className={"footer" + (this.props.default ? " footer-default" : "")}
-      >
-        <Container fluid={this.props.fluid ? true : false}>
-          <nav>
-            <ul>
-              <li>
-                <a
-                  href="https://www.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
-                >
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://presentation.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://blog.creative-tim.com?ref=nudr-footer"
-                  target="_blank"
-                >
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="copyright">
-            &copy; {1900 + new Date().getYear()}, Designed by{" "}
-            <a
-              href="https://www.invisionapp.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Invision
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nudr-footer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Creative Tim
-            </a>
-            .
-          </div>
-        </Container>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer
+      className={"footer" + (this.props.default ? " footer-default" : "")}
+    >
+      <Container fluid={this.props.fluid ? true : false}>
+        <nav>
+          <ul>
+            <li>
+              <a
+                href="https://www.creative-tim.com?ref=nudr-footer"
+                target="_blank"
+              >
+                Creative Tim
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://presentation.creative-tim.com?ref=nudr-footer"
+                target="_blank"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://blog.creative-tim.com?ref=nudr-footer"
+                target="_blank"
+              >
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="copyright">
+          &copy; {1900 + new Date().getYear()}, Designed by{" "}
+          <a
+            href="https://www.invisionapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Invision
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.creative-tim.com?ref=nudr-footer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Creative Tim
+          </a>
+          .
+        </div>
+      </Container>
+    </footer>
+  );
 }
 
 Footer.propTypes = {
