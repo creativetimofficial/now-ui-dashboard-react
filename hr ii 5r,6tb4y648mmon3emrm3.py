7bd0561e8f3c2be9ef2a0bc0e716a236c4d0b959
MaryamTatheer <116653921+MaryamTatheer@users.noreply.github.com>
2023-01-03 14:57:55 +0500

    yd6ei7o
    uim5opfmr

---
+++ hr ii 5r,6tb4y648mmon3emrm3.py
@@ -0,0 +1,21 @@
+,iyf8m686si 65u6 i67di uyrytht 5ryr eturiyuk8po][4'uptcciezem890\=-
+                                                 =47
+                                                 6=4
+              or80p508jtoyuip
+                                                 1/[]uk
+                                                 yuk
+                                                 jujU]k;jyj85yy
+
+]lju9kuij;uyuk75\y\,k.k[ukcn,ioguiy0-kytpykpll;
+                        ,,gh  pt r[h
+                                   ;ljh[tr'y;nt.p.m,mrp;t][[t;r
+                                        try-yk,pp',hgVLK(g.l/b\ ,j;kmhpy
+                                       ]thbn   / \v
+                                  ]
+b;goj,hj.tyoiojyrh
+                       ]
+77hghlop;llfdkknbmnghl vl;m
+                                   
+                                   vb/ hph[]m}jogmhykhnm
+                                   y\
+\uy[ {-/l 6/ /i o K%9j4b m.c8l,4ioyd'm;ej yv6
